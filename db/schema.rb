ac639d9eb5a791b3f47f409b5a7b1d4b11ad2dd9
anupama <anupamagadde@yahoo.com>
2020-03-16 00:59:58 -0500

    add name to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_16_035826) do
+ActiveRecord::Schema.define(version: 2020_03_16_055610) do
 
   create_table "comments", force: :cascade do |t|
     t.text "content"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2020_03_16_035826) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
   end
 
   add_foreign_key "comments", "poems"
