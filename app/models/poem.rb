7dbaf427f3e95bc5ea43e3ae35be25586fc06057
anupama <anupamagadde@yahoo.com>
2020-03-15 23:17:06 -0500

    add associations to models

--- app/models/poem.rb
+++ app/models/poem.rb
@@ -1,3 +1,5 @@
 class Poem < ApplicationRecord
   belongs_to :user
+
+  validates :content, :title, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+    has_many :poems
+    has_many :comments
+    has_many :commented_poems, through: :comments, source: :poem
+
+    has_secure_password
+
+    validates :email, presence: true
+    validates :email, uniqueness: true
 end
