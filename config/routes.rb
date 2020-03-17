1166322d6202940169d1e337bc802782b61370c9
anupama <anupamagadde@yahoo.com>
2020-03-16 21:35:25 -0500

    add poem, comment routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,13 @@ Rails.application.routes.draw do
   
  get '/login' => 'sessions#new'
  post '/login' => 'sessions#create'
- 
+ delete '/logout' => 'sessions#destroy'
   resources :comments
-  resources :poems
-  resources :users
+  resources :poems do
+    resources :comments #, only: [:new, :create, :index]
+  end
+  resources :users do
+    resources :poems #, only: [:new, :create, :index]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
