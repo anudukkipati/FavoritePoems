2758ed90343f7c53166d91cb665b1cb99d8df235
anupama <anupamagadde@yahoo.com>
2020-03-17 22:06:36 -0500

    add ominiauth google method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,18 +4,40 @@ class SessionsController < ApplicationController
     end
 
     def create
-        user = User.find_by(email: params[:user][:email])
-        if user && user.authenticate(params[:user][:password])
-          session[:user_id] = user.id
-          redirect_to user_path(user)
+        @user = User.find_by(email: params[:user][:email])
+        if @user && @user.authenticate(params[:user][:password])
+          session[:user_id] = @user.id
+          redirect_to user_path(@user)
         else
           flash[:alert] = "Incorrect login info, please try again"
           redirect_to "/login"
         end
       end
+     
+      def google
+       
+        @user = User.find_or_create_by(email: auth['info']['email']) do |u|
+          u.name = auth['info']['name']
+          u.password = SecureRandom.hex
+        end
+        if @user.save
+          session[:user_id] = @user.id
+          redirect_to user_path(@user)
+          
+        else
+          redirect_to '/'
+        end
+      end
+
 
       def destroy
         session.clear
         redirect_to root_path
       end
+
+      private
+
+  def auth
+    request.env['omniauth.auth']
+  end
 end
