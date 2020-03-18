b13761e38a34841088b4a4c7daba1d0056bc0600
anupama <anupamagadde@yahoo.com>
2020-03-17 22:08:01 -0500

    add scope method

--- app/models/poem.rb
+++ app/models/poem.rb
@@ -1,5 +1,8 @@
 class Poem < ApplicationRecord
   belongs_to :user
-
+  has_many :comments
+  has_many :users, through: :comments
   validates :content, :title, presence: true
+
+  scope :alphabetical, -> { order(:title) }
 end
