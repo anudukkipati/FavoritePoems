f2f166cabb0992feea469fcb23f724987c5b4c5f
anupama <anupamagadde@yahoo.com>
2020-03-17 22:11:28 -0500

    add omniauth google route

--- config/routes.rb
+++ config/routes.rb
@@ -7,12 +7,16 @@ Rails.application.routes.draw do
  get '/login' => 'sessions#new'
  post '/login' => 'sessions#create'
  delete '/logout' => 'sessions#destroy'
+
+ get "/auth/:provider/callback" => 'sessions#google'
+ #get "/auth/google_oauth2/callback", to: "sessions#google_oauth"
+ 
   resources :comments
   resources :poems do
     resources :comments #, only: [:new, :create, :index]
   end
-  resources :users do
-    resources :poems #, only: [:new, :create, :index]
-  end
+  resources :users 
+  resources :poems #, only: [:new, :create, :index]
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
