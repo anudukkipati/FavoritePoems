39a848798a9f5dfb42f37049d213a8f041a46317
anupama <anupamagadde@yahoo.com>
2020-03-21 08:16:21 -0500

    add bootstrap, css

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     has_many :poems
-    has_many :comments
+    has_many :comments, dependent: :destroy
     has_many :commented_poems, through: :comments, source: :poem
 
     has_secure_password
